1435652714c218c9ceab521d284830187417e66c
DansGamer <dans.oficial@gmail.com>
2026-03-16 22:19:11 -0300

    Adicionando NomeDoColega

--- cloneGit.cpp
+++ cloneGit.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
 #include <locale>
+#include <string>
 
 using namespace std;
 
 int main() {
+    string NomeDoColega = "Daniel";
     setlocale(LC_ALL, "pt_BR.utf8"); //Para usar acentuação
     cout << "\033[1;34m***************************************************\033[0m\n";
     cout << "\033[1;32m*                                                 *\033[0m\n";
-    cout << "\033[1;32m*   \033[1;33m PARABÉNS 34DS2K26!                     \033[1;32m          *\033[0m\n";
+    cout << "\033[1;32m*   \033[1;33m PARABÉNS 34DS2K26!                     \033[1;32m      *\033[0m\n";
     cout << "\033[1;32m*                                                 *\033[0m\n";
     cout << "\033[1;32m*   \033[1;36mVocê clonou um repositório público do Git     \033[1;32m*\033[0m\n";
     cout << "\033[1;32m*   \033[1;36mpara o seu repositório local!                 \033[1;32m*\033[0m\n";
